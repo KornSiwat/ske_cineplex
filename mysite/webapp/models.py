e5c43a714570b1a18fd450c1e35090540c8be9e8
KornSiwat <ponpued@gmail.com>
2018-11-23 16:54:40 +0700

    created theater and movie

--- mysite/webapp/models.py
+++ mysite/webapp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    name = models.CharField(max_length=50)
+
+    CATEGORIES_CHOICE = (
+        ('GE', 'General'),
+        ('AC', 'Action'),
+        ('AD', 'Adults'),
+        ('AV', 'Adventure'),
+        ('HR', 'Horror'),
+        ('KD', 'Kids'),
+    )
+    categorie = models.CharField(
+        max_length=2,
+        choices=CATEGORIES_CHOICE,
+        default='GE',
+    )
+
+    def __str__(self):
+        return self.name
+
+class Theater(models.Model):
+    theater_id = models.CharField(max_length=10)
+    movie = models.ForeignKey(Movie,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.theater_id} showing: {self.movie}'

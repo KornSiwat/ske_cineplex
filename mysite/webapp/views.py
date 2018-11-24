c1f106c9cd583387bdb051af1c6d4ddcb485ebcb
KornSiwat <ponpued@gmail.com>
2018-11-25 05:23:29 +0700

    menu in navbar changed based on route

--- mysite/webapp/views.py
+++ mysite/webapp/views.py
@@ -3,4 +3,5 @@ from .models import Theater
 
 def index(request):
     theater_lists = Theater.objects.all()
-    return render(request, 'webapp/index.html',{'theater_lists' : theater_lists})
+    return render(request, 'webapp/index.html',{'theater_lists' : theater_lists, 'route' : 'showtime',
+    })

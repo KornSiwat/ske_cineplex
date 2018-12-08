2538ad534345193bf9a7f1196b5e8c6ae0bedf17
KornSiwat <ponpued@gmail.com>
2018-12-08 17:29:16 +0700

    fix js

--- webapp/static/js/main.js
+++ webapp/static/js/main.js
@@ -1,27 +1,31 @@
+$( document ).ready(function() {
 
-const select = function(elem) {
-  let movie_field = $('#movie-field');
-  let movie_name = $('.booking-movie-name');
-  movie_field[0].value = movie_name[0].id
-  let date_field = $('#date-field');
-  let today = $('.date');
-  date_field[0].value = today[0].id
-  let input = $('#seat-input-box');
-  if (elem.src === `http://${location.host}/static/img/free.png`) {
-    elem.src = `http://${location.host}/static/img/selected.png`;
-    if (input[0].value.length == 0) {
-      input[0].value += elem.id;
+  $('.seat-icon-aval').click(function(){
+    let elem = $(this);
+    let movie_field = $('#movie-field');
+    let movie_name = $('.booking-movie-name');
+    movie_field[0].value = movie_name[0].id
+    let date_field = $('#date-field');
+    let today = $('.date');
+    date_field[0].value = today[0].id
+    let input = $('#seat-input-box');
+    if (elem[0].src === `http://${location.host}/static/img/free.png`) {
+      elem[0].src = `http://${location.host}/static/img/selected.png`;
+      if (input[0].value.length == 0) {
+        input[0].value += elem[0].id;
+      }
+      else {
+        input[0].value += `,${elem[0].id}`;
+      }
     }
-    else {
-      input[0].value += `,${elem.id}`;
-    }
-  }
-  else if (elem.src === `http://${location.host}/static/img/selected.png`)  {
-    elem.src = `http://${location.host}/static/img/free.png`
-    input[0].value = input[0].value.replace(`,${elem.id}`, "");
-    input[0].value = input[0].value.replace(`${elem.id},`, "");
-    input[0].value = input[0].value.replace(`${elem.id}`, "");
-}}
+    else if (elem[0].src === `http://${location.host}/static/img/selected.png`)  {
+      elem[0].src = `http://${location.host}/static/img/free.png`
+      input[0].value = input[0].value.replace(`,${elem[0].id}`, "");
+      input[0].value = input[0].value.replace(`${elem[0].id},`, "");
+      input[0].value = input[0].value.replace(`${elem[0].id}`, "");
+  }});
+});
+
 
 const choose_time = function(elem) {
   let all_showtime = $(".showtime-button");
@@ -45,6 +49,21 @@ const choose_time = function(elem) {
   })
 }
 
+const update_history = function() {
+  let name = $('#history-name');
+  let tel = $('#history-tel');
+  $.ajax({
+    url: `/update_history/${name[0].value}/${tel[0].value}`,
+    method: 'GET',
+    data: {},
+    success: function(data){
+      $('#history-update').html(data);
+    }, error: function(error){
+      console.log('error')
+    }
+  })
+}
+
 const check_card = function() {
   let card = $('.card');
   let winWidth = (window.innerWidth - (0.1* window.innerWidth));
@@ -63,18 +82,3 @@ setTimeout(check_card, 0);
 window.onresize = function() {
   check_card();
 };
-
-const update_history = function() {
-  let name = $('#history-name');
-  let tel = $('#history-tel');
-  $.ajax({
-    url: `/update_history/${name[0].value}/${tel[0].value}`,
-    method: 'GET',
-    data: {},
-    success: function(data){
-      $('#history-update').html(data);
-    }, error: function(error){
-      console.log('error')
-    }
-  })
-}
